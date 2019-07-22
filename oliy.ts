dbfc6d79208f8828df39916319355ead0dd40f3e
Ken <kyrolites@gmail.com>
2019-07-23 02:30:10 +0800

    oliy
    oliy

---
+++ oliy.ts
@@ -0,0 +1,22 @@
+export namespace Oliy {
+	export const oliy = "fat";
+	export class Oliy extends class extends class {
+		constructor() {
+			return class Oliy {
+
+			}
+		}
+	}{
+		constructor() {
+			super();
+			return oliy;
+		}
+	}{
+		constructor() {
+			super();
+		}
+		isFat() {
+			return true;
+		}
+	}
+}
